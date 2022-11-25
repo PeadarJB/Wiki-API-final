bb117cb308044e51c5e7a737e27635849b888d51
Peadar Jolliffe-Byrne <peadarjb@gmail.com>
2022-11-24 20:35:22 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,120 @@
+// Server starting code
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// Setting up mongoDB database
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/wikiDB");
+}
+// mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+// Chained methods. Requests targetting all articles!
+app.route("/articles")
+
+.get(function(req, res) {
+  Article.find({}, function(err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+// Requests targetting a specific article
+
+app.route("/articles/:articleTitle")
+
+  .get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function (err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No items matching that article found.");
+    }
+  });
+})
+// Use .put and replaceOne to update the entire document.
+.put(function(req, res) {
+  Article.replaceOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    function (err){
+      if (!err) {
+        res.send("Successfully updated article.");
+      }
+    }
+  );
+})
+// Use .patch and updateOne to edit just one field in the object.
+.patch(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title},
+    function (err){
+      if (!err) {
+        res.send("Successfully updated article.");
+      }
+    }
+  );
+})
+
+.delete(function(req, res) {
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function (err){
+      if (!err) {
+        res.send("Item successfully deleted.")
+      }
+    }
+  );
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
